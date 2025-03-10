035b9a8525a2874bdc67aa5481fc9c2eb171146f
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-10 18:39:30 +0300

    100325

--- src/test/java/day01/HesapMak.java
+++ src/test/java/day01/HesapMak.java
@@ -1,4 +1,16 @@
 package day01;
 
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import org.testng.annotations.Test;
+
 public class HesapMak {
+
+    AndroidDriver<AndroidElement> driver;
+
+    @Test
+    public void hesapMakinesi(){
+        // Kullanici gerekli kurulumları yapar
+
+    }
 }

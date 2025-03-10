67084b2c0a75e182959984aa7b268578e9d42647
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-10 19:01:45 +0300

    100325

--- src/test/java/day01/HesapMak.java
+++ src/test/java/day01/HesapMak.java
@@ -7,6 +7,7 @@ import org.testng.annotations.Test;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.util.concurrent.TimeUnit;
+import static org.testng.Assert.*;
 
 public class HesapMak {
 
@@ -22,8 +23,12 @@ public class HesapMak {
         capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
         capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
         capabilities.setCapability(MobileCapabilityType.APP, "D:\\Java\\SeleniumProject\\HmzAppium\\Apps\\Calculator_8.4 (503542421)_Apkpure (3).apk");
+        // capabilities.setCapability(MobileCapabilityType.APP,"D:\\Java\\SeleniumProject\\HmzAppium\\Apps\\Apk Bilgisi_2.3.4_apkcombo.com.apk");
 
         driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
         driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
+
+        // Uygulamanın yüklendigi dogrulanır
+        assertTrue(driver.isAppInstalled("com.google.android.calculator"));
     }
 }

52f78a54e25d248e6828df1ca2042d2904a8ae98
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-10 18:50:29 +0300

    100325

--- src/test/java/day01/HesapMak.java
+++ src/test/java/day01/HesapMak.java
@@ -1,16 +1,29 @@
 package day01;
-
 import io.appium.java_client.android.AndroidDriver;
 import io.appium.java_client.android.AndroidElement;
+import io.appium.java_client.remote.MobileCapabilityType;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.testng.annotations.Test;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
 public class HesapMak {
 
     AndroidDriver<AndroidElement> driver;
 
     @Test
-    public void hesapMakinesi(){
+    public void hesapMakinesi() throws MalformedURLException {
         // Kullanici gerekli kurulumları yapar
+        DesiredCapabilities capabilities = new DesiredCapabilities();
 
+        capabilities.setCapability(MobileCapabilityType.DEVICE_NAME, "Pixel 148");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_NAME, "Android");
+        capabilities.setCapability(MobileCapabilityType.PLATFORM_VERSION, "10.0");
+        capabilities.setCapability(MobileCapabilityType.AUTOMATION_NAME, "UiAutomator2");
+        capabilities.setCapability(MobileCapabilityType.APP, "D:\\Java\\SeleniumProject\\HmzAppium\\Apps\\Calculator_8.4 (503542421)_Apkpure (3).apk");
+
+        driver = new AndroidDriver<AndroidElement>(new URL("http://127.0.0.1:4723/wd/hub"), capabilities);
+        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
     }
 }

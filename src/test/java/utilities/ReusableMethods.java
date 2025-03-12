ca5d2d460a5d895872998b93b4569c5120c81646
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-12 18:55:52 +0300

    120325

---
+++ src/test/java/utilities/ReusableMethods.java
@@ -0,0 +1,17 @@
+package utilities;
+
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.WaitOptions;
+import io.appium.java_client.touch.offset.PointOption;
+
+import java.time.Duration;
+
+public class ReusableMethods {
+
+    public static void koordinatTiklamaMethodu(int xkoordinati, int ykoordinati, int beklemeSuresi){
+        TouchAction action = new TouchAction<>(Driver.getAndroidDriver());
+        action.press(PointOption.point(xkoordinati,ykoordinati))
+                .waitAction(WaitOptions.waitOptions(Duration.ofMillis(beklemeSuresi))).release().perform();
+    }
+
+}

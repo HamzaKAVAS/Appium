da0aba4c236566fbcafec9b62098f3ab4941d083
HamzaKAVAS <hmzkavas@gmail.com>
2025-03-14 18:34:20 +0300

    140325

---
+++ src/test/java/tests/day03/Kiwi.java
@@ -0,0 +1,25 @@
+package tests.day03;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.AndroidElement;
+import pages.KiwiPage;
+import utilities.Driver;
+
+public class Kiwi {
+
+    AndroidDriver<AndroidElement> driver = Driver.getAndroidDriver();
+    KiwiPage page = new KiwiPage();
+
+    // uygulamanin yuklendigi dogrulanir
+    // uygulamanin basariyla acildigi dogrulanir
+    // misafir olarak devam et e tiklanir
+    // ardindan gelecek olan 3 adimda da yesil butona basilarak devam edilir
+    // Trip type,one way olarak secilir
+    // kalkis ulkesi secenegine tiklanir ve default olan ulke kaldirilir
+    // kalkis yapilacak ulke/sehir girilir ve sec e tiklanir
+    // varis ulkesi secenegine tiklanir ve gidilecek ulke girilir
+    // gidis tarihi 28 Mart olarak secilir ve set date e tiklanir
+    // search butonuna tiklanir
+    // en ucuz ve aktarmasiz filtrelemeleri yapilir
+    // gelen bilet fiyati kaydedilir ve kullanicin telefonuna sms olarak gonderilir
+}
